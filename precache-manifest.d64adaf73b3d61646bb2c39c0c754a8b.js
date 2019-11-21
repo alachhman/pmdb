b2e2907b2821553f84fd23dcc69d749521478060
Anthony <lachhman.anthony@gmail.com>
2019-11-21 14:13:38 -0500

    Updates

---
+++ precache-manifest.d64adaf73b3d61646bb2c39c0c754a8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0ae2c121031b13629d50d679261373e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfef76396a8b48a76627",
+    "url": "/static/css/2.5ccf0df5.chunk.css"
+  },
+  {
+    "revision": "ea5502af20a581eb5a89",
+    "url": "/static/css/main.f75d77a5.chunk.css"
+  },
+  {
+    "revision": "bfef76396a8b48a76627",
+    "url": "/static/js/2.bdb4eb2d.chunk.js"
+  },
+  {
+    "revision": "ea5502af20a581eb5a89",
+    "url": "/static/js/main.add987d0.chunk.js"
+  },
+  {
+    "revision": "1595ead9f38bb416c21d",
+    "url": "/static/js/runtime-main.a2980942.js"
+  }
+]);
